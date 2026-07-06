f8c96e063f7046548924d043d8b62be0a78f3890
hetsakariya2007 <hetsakariya.0111@gmail.com>
2026-07-06 17:07:31 +0530

    feat(frontend): add reviews reducer to store, add Reviews route, update Home page

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
+import reviewReducer from './reviewSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    reviews: reviewReducer,
   },
 });
