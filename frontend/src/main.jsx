ae4235a31627cf2be0c0a5c0e5d6d696f6416658
hetsakariya2007 <hetsakariya.0111@gmail.com>
2026-07-06 15:37:20 +0530

    feat(frontend): update main.jsx, add ProtectedRoute and Header components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { HelmetProvider } from 'react-helmet-async'
+import { Toaster } from 'react-hot-toast'
 import './index.css'
 import App from './App.jsx'
+import { store } from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <HelmetProvider>
+        <App />
+        <Toaster />
+      </HelmetProvider>
+    </Provider>
   </StrictMode>,
 )
